20fdd621b44e45a36783717d5ac250f426cd9c54
Andrew Brown <ajbpn7@mst.edu>
2012-05-23 00:20:29 -0500

    Woeful is me, for I have sinned once more

--- igor-lib.js
+++ igor-lib.js
@@ -1,43 +1,43 @@
 function parse_input(utterances)
 {
-	for (i = 0; i < utterances.length && i < igor_config.utterance_match_limit; i++)
-	{
-		utterance = utterances[i].utterance;
-		debug("Probably said: " + utterance);
-		
-		for (c in igor_commands)
-		{
-			command = igor_commands[c];
-			
-			if (command.matcher(utterance, command.keywords))
-			{
-				debug("Invoking " + command.name);
-			}
-		}
-	}
+  for (i = 0; i < utterances.length && i < igor_config.utterance_match_limit; i++)
+  {
+    utterance = utterances[i].utterance;
+    debug("Probably said: " + utterance);
+    
+    for (c in igor_commands)
+    {
+      command = igor_commands[c];
+      
+      if (command.matcher(utterance, command.keywords))
+      {
+        debug("Invoking " + command.name);
+      }
+    }
+  }
 }
-	
+  
 function match_all (haystack, needles)
 {
-	for (i in needles)
-	{
-		pedantic("Trying to match " + needles[i] + " in " + haystack);
-		if (haystack.toLowerCase().indexOf(needles[i].toLowerCase()) < 0)
-		{
-			return false;
-		}
-	}
-	return true;
+  for (i in needles)
+  {
+    pedantic("Trying to match " + needles[i] + " in " + haystack);
+    if (haystack.toLowerCase().indexOf(needles[i].toLowerCase()) < 0)
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
 function match_any (haystack, needles)
 {
-	for (i in needles)
-	{
-		if (haystack.indexOf(needles[i]) > -1)
-		{
-			return true;
-		}
-	}
-	return false;
+  for (i in needles)
+  {
+    if (haystack.indexOf(needles[i]) > -1)
+    {
+      return true;
+    }
+  }
+  return false;
 }
--- igor.js
+++ igor.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-	$('#igor-input').bind('onwebkitspeechchange', function (e)
-	{
-		parse_input(e.results);
-	});
+  $('#igor-input').bind('onwebkitspeechchange', function (e)
+  {
+    parse_input(e.results);
+  });
 });
