ad278e3ad61d3681ee3b12bd7fb599adc1f561b0
Andrew Brown <ajbpn7@mst.edu>
2012-05-23 00:29:04 -0500

    You know, I'm a rather brilliant surgeon. Perhaps I can help you with that hump.

--- igor.js
+++ igor.js
@@ -1,6 +1,14 @@
-$(document).ready(function () {
-  $('#igor-input').bind('onwebkitspeechchange', function (e)
-  {
-    parse_input(e.results);
-  });
-});
+loadScript('igor-config.js');
+loadScript('igor-debug.js');
+loadScript('igor-lib.js');
+loadScript('igor-commands.js');
+loadScript('igor-initializer.js');
+
+function loadScript(url)
+{
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
+   head.appendChild(script);
+}
